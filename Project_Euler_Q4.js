38520c5dd59aa02f48e1b3d151bb3dcc53234db2
Spencer Allen <spencer@apexhire.com>
2013-04-30 18:45:17 -0700

    Update Project_Euler_Q4.js

--- Project_Euler_Q4.js
+++ Project_Euler_Q4.js
@@ -1,3 +1,9 @@
+/*
+The problem:
+A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 99.
+Find the largest palindrome made from the product of two 3-digit numbers.
+*/
+
 // this is a function that takes a string and reverses it. 
 function reverse(s) {
     return s.split("").reverse().join("");
